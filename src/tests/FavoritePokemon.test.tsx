4b9f63943988d336437429660a5acde51081fa8c
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-09-04 00:57:38 -0300

    implementando testes da página de favoritos

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,28 @@
-test('', () => {});
+import { screen, render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import { FavoritePokemon } from '../pages';
+
+describe('Switch de teste do requisito 3', () => {
+  it('Testa se uma mensagem específica é exibida quando não há pokemon favorito', () => {
+    render(<FavoritePokemon />);
+    const notFavorite = screen.getByText(/no favorite pokémon found/i);
+    expect(notFavorite).toBeInTheDocument();
+  });
+
+  it('Testa se uma mensagem específica compatível é exibida para um pokemon favoritado', async () => {
+    const { user } = renderWithRouter(<App />);
+    const details = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    await user.click(details);
+    const favoritePokemon = screen.getByText(/pokémon favoritado\?/i);
+    await user.click(favoritePokemon);
+    const favLink = screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    });
+    await user.click(favLink);
+    const pikachu = screen.getByText(/pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+  });
+});
