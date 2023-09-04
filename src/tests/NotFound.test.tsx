d932af45ecd9965232f19b40378c401e02a8d9a6
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-09-04 01:43:06 -0300

    implementando testes para componente NotFound

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Switch de teste do requisito 4 - Componente NotFound', () => {
+  it('testando se no componente contem um testo do tipo h2 com um texto específico', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading', {
+      name: /page requested not found/i, level: 2,
+    });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('testando se no componente contem uma imagem com texto alternativo', () => {
+    renderWithRouter(<NotFound />);
+    const alt = screen.getByAltText(
+      'Clefairy pushing buttons randomly with text I have no idea what i\'m doing',
+    );
+    expect(alt).toBeInTheDocument();
+  });
+});
